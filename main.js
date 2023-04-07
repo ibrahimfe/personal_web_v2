ebe02f5585e08307a887bac6c560ac14d98ce3dc
ibrahimfe <ibrahiimm.f@gmail.com>
2023-04-07 23:31:07 +0700

    updating the html file, the main.css file, and adding new image

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-function menubar() {}
+const navbarnav = document.querySelector(".navbar-menu");
+document.querySelector("#menubar").onclick = function () {
+  navbarnav.classList.toggle("active");
+};
+
+const menubar = document.querySelector("#menubar");
+document.addEventListener("click", function (e) {
+  if (!menubar.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+});
